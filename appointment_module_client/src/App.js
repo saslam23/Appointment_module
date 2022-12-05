b491db4709714bbc5b529ed3c21ba17541e7821c
Saad Aslam <saslam@intouchconnections.com>
2022-12-05 13:30:15 -0600

    first commit

---
+++ appointment_module_client/src/App.js
@@ -0,0 +1,14 @@
+import Stepper from './components/Stepper';
+function App() {
+  return (
+    <div className="App">
+      <div style={{display:'flex',flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
+        <h1>Appointment Mod</h1>
+        <Stepper/>
+        </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ appointment_module_client/src/components/Step1.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Step1 () {
+    return(
+        <>
+        <div>
+            <ul>
+                <li>
+                    Studio 786 salon
+                </li>
+                <li>
+                    Illy's
+                </li>
+            </ul>
+        </div>
+        </>
+    )
+}
+
+export default Step1;
